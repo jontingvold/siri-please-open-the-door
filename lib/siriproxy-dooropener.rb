2f4f3c88685d87c7781ff5fedb9acaa59486a31b
Jon Tingvold <jon.tingvold@gmail.com>
2013-04-05 02:13:53 +0200

    Remove some comments

--- lib/siriproxy-dooropener.rb
+++ lib/siriproxy-dooropener.rb
@@ -4,10 +4,6 @@ require 'pp'
 require 'open-uri'
 
 #######
-# This is a "hello world" style plugin. It simply intercepts the phrase "test siri proxy" and responds
-# with a message about the proxy being up and running (along with a couple other core features). This
-# is good base code for other plugins.
-#
 # Remember to add other plugins to the "config.yml" file if you create them!
 ######
 
